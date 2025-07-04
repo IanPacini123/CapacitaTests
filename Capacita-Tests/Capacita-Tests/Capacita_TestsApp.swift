ee7bfb0fb52e0221ddcccd6072f1ec79c2f755a9
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2025-07-04 15:13:44 -0300

    setup do projeto

---
+++ Capacita-Tests/Capacita-Tests/Capacita_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capacita_TestsApp.swift
+//  Capacita-Tests
+//
+//  Created by Ian Pacini on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Capacita_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
